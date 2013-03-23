6d9d10cc621dcd3009537cba44164a09c3d85d91
cakesy <john@phlogy.com>
2013-03-23 15:24:48 +0100

    Initial Commit

---
+++ iOsAntiPiracy/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOsAntiPiracy
+//
+//  Created by JohnCakes on 23/03/13.
+//  Copyright (c) 2013 JohnCakes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iOsAntiPiracy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOsAntiPiracy
+//
+//  Created by JohnCakes on 23/03/13.
+//  Copyright (c) 2013 JohnCakes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
